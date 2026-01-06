94fd39c1dd5ad0bfb0afa717e51bba83652f357f
chellakumar@asus <ckchellakumar2003@gmail.com>
2026-01-06 11:46:27 +0530

    update: All the newcessary configurations are done! [fastify,config for env,validationPipes,Loggers]

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { defineDmmfProperty } from 'generated/prisma/runtime/client';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const port = process.env.PORT ?? 3000;
+  await app.listen({ port: Number(process.env.PORT) || 3000, host: '0.0.0.0' });
+  Logger.log(`Server running on http://localhost:${port}`);
+  Logger.log("Application Adapter: "+app.getHttpAdapter().getType());
 }
 bootstrap();
